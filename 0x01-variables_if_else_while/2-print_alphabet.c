a99e012fc67e3f7bbcc9ff94a0174a8561230090
Francesca <ukohmsy@gmail.com>
2023-06-16 15:23:42 -0700

    if else vafriables

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
 
-int main() {
+int main(void) {
     char ch;
 
     for (ch = 'a'; ch <= 'z'; ch++) {
